97da4c3acd43436e133ad722d8dd3a688b96ed96
Damian Kress <info@damiankress.de>
2020-01-25 04:02:30 +0100

    ..

--- api/routes/photos.js
+++ api/routes/photos.js
@@ -9,7 +9,7 @@ const multer = require('multer')
 
 const photoStorage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, __dirname + '/public/media/photos/')
+		cb(null, __dirname + '/../../public/media/photos/')
 	},
 	filename: function (req, file, cb) {
 		let fileName = new Date().toISOString().replace(/[^a-z0-9]/gi, '') + '.' + file.originalname
@@ -19,7 +19,7 @@ const photoStorage = multer.diskStorage({
 })
 const avatarStorage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, __dirname + '/public/media/avatars/')
+		cb(null, __dirname + '/../../public/media/avatars/')
 	},
 	filename: function (req, file, cb) {
 		let fileName = new Date().toISOString().replace(/[^a-z0-9]/gi, '') + '.' + file.originalname
