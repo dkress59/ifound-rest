39831fa1c3800d9cea04835585101caadf7a1ca4
Damian Kress <info@damiankress.de>
2020-01-27 00:28:28 +0100

    auth

--- api/auth/check.js
+++ api/auth/check.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
 	const token = /* (req.headers.authorization.split(" ")[0] === 'Bearer')
-		? */ req.headers.Authorization.split(" ")[1]
-		//: req.body.token
+		? */ req.headers.authorization.split(" ")[1]
+	//	: req.body.token
 	console.log('token', token)
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_KEY)// verifies AND decodes
-		req.userData = decoded// attaches userData to future requests
+		//req.userData = decoded// attaches userData to future requests
 		next()
 	} catch (err) {
 		return res.status(401).json({
