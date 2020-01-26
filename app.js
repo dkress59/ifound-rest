e8b582b0dfaac446576fdcda6105187cdacf22c8
Damian Kress <info@damiankress.de>
2020-01-27 00:37:09 +0100

    allow OPIONS

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 	)
 	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+		res.header('Access-Control-Allow-Methods', 'OPTIONS, PUT, POST, PATCH, DELETE, GET')
 		return res.status(200).json({})
 	}
 	next()
