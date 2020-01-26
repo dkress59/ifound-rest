fc162f0841ce96cf5285e1210b1648174d5b2e4e
Damian Kress <info@damiankress.de>
2020-01-26 15:26:07 +0100

    tring to get cb from post to /photos

--- api/routes/places.js
+++ api/routes/places.js
@@ -143,7 +143,7 @@ router.post('/', upload.single('photoData'), (req, res, next) => {
 					'place': result._id
 				})
 			}).then(response => {
-				console.log('retreive _id from:', response)
+				console.log('retreive _id from:', response.newPhoto)
 			})
 		})
 		.catch(err => {
