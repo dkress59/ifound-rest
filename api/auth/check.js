baac9ac504011c396c467a05808d31a6562099f5
Damian Kress <info@damiankress.de>
2020-01-27 00:32:19 +0100

    +log

--- api/auth/check.js
+++ api/auth/check.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
+	console.log('auth headers', req.headers)
 	const token = /* (req.headers.authorization.split(" ")[0] === 'Bearer')
 		? */ req.headers.authorization.split(" ")[1]
 	//	: req.body.token
