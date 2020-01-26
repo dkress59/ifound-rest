f64f52c63fe29c75ade4bde1f47c7629cfe653b8
Damian Kress <info@damiankress.de>
2020-01-27 00:10:43 +0100

    Refined authorisation.

--- api/auth/check.js
+++ api/auth/check.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
+	const token = (req.headers.authorization.split(" ")[0] === 'Bearer')
+		? req.headers.authorization.split(" ")[1]
+		: req.body.token
 	try {
-		const token = (req.headers.authorization.split(" ")[0] === 'Bearer')
-			: req.headers.authorization.split(" ")[1]
-			? req.body.token
 		const decoded = jwt.verify(token, process.env.JWT_KEY)// verifies AND decodes
 		req.userData = decoded// attaches userData to future requests
 		next()
