3cba6d17d5a4d9959f8a8b80603dce15f62205c7
Damian Kress <info@damiankress.de>
2020-01-27 00:16:16 +0100

    token problem?

--- api/auth/check.js
+++ api/auth/check.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next) => {
 	const token = (req.headers.authorization.split(" ")[0] === 'Bearer')
 		? req.headers.authorization.split(" ")[1]
 		: req.body.token
+	console.log('token', token)
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_KEY)// verifies AND decodes
 		req.userData = decoded// attaches userData to future requests
