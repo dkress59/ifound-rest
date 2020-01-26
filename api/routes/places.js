24e4aae460a6e1b2eaee8cbab1a59b5f29c964e7
Damian Kress <info@damiankress.de>
2020-01-26 11:38:45 +0100

    forgot fileFilter

--- api/routes/places.js
+++ api/routes/places.js
@@ -10,6 +10,15 @@ const Photo = require('../models/photo')
 const auth = require('../auth/check')
 
 const multer = require('multer')
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'
+		) {
+		cb(null, true)
+	} else {
+		cb(null, false)
+	}
+}
 const upload = multer({
 	limits: {
 		fileSize: 1024 * 1024 * 2.5
