b077cb9fd7b6b693b08a365f52eb72e19a5df26e
Damian Kress <info@damiankress.de>
2020-03-07 12:06:52 +0100

    added (redundant?) json compression

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ mongoose.connect(
 )
 
 
+app.set('json spaces', 0)
+
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Credentials", "true")
