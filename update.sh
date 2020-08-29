f5f1f57fd7081b53f47a3c9a225ef971f582fd33
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-29 15:23:11 +0200

    npm bug fix 1

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 cd /var/www/ifound-rest
 /usr/bin/git fetch origin master
 /usr/bin/git reset --hard origin/master
-/usr/bin/npm install /var/www/ifound-rest/package.json
+npm install
 #/usr/local/bin/pm2 restart ifound-rest
