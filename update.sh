03c85df8cbc2f2ee0958798a967c752c4924eb0d
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-29 15:41:13 +0200

    pipeline fix

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 cd /var/www/ifound-rest
 /usr/bin/git fetch origin master
 /usr/bin/git reset --hard origin/master
-ls /usr/bin
-ls /usr/local/bin
-#npm install
-#/usr/local/bin/pm2 restart ifound-rest
+/usr/bin/npm install
