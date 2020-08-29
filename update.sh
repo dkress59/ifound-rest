5e2fa20dc5ceb248cb3101bcc1ae88da7b32a8c5
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-29 16:00:34 +0200

    last try

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 cd /var/www/ifound-rest
 /usr/bin/git fetch origin master
 /usr/bin/git reset --hard origin/master
-rm -rf /var/www/ifound-rest/node_modules /var/www/ifound-rest/package-lock.json
-/usr/bin/npm run --silent install
+/usr/bin/npm install --silent
